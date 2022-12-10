f02b71f9f56657cdc8be37338a5e19af65866ee4
Yewande Adeyeye <Yewande Adeyeye>
2022-12-09 21:06:24 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *	main - alphabets
+ *
+ *
+ *
+ *	Return: 0
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
