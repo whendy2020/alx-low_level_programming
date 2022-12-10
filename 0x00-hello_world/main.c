bfafea156edfc71fa3985df26f8fcc297af1f0d5
Yewande Adeyeye <Yewande Adeyeye>
2022-12-10 09:53:25 -0800

    Betty style preprocess c

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,13 +1,12 @@
-/*
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *
- */
 #include <stdio.h>
+/**
+ *	main - Entry point
+ *
+ *
+ *
+ *
+ *	Return: Always 0 (Success)
+*/
 int main(void)
 {
 	return (0);
