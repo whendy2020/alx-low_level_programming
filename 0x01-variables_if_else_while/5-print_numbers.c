e08f41d2627eaba13f59e341b4ec3dca805585d1
Yewande Adeyeye <Yewande Adeyeye>
2022-12-11 06:41:55 -0800

    print binary 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *	main - num from 0 to 9
+ *
+ *
+ *
+ *	Return: 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
