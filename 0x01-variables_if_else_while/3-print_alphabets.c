be6b10853ff6619b47ad8cb4e8508d754c8713b4
Yewande Adeyeye <Yewande Adeyeye>
2022-12-11 04:58:12 -0800

    putchar print low and upper alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *	main - upper and lower alphabet using putchar
+ *
+ *
+ *
+ *
+ *	Return: 0
+*/
+int main(void)
+{
+	char low;
+	char upper;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		putchar(upper);
+	}
+	return (0);
+}
