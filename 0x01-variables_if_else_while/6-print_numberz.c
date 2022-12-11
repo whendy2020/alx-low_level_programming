1f6bd12bb13f680ca766ff16a6190da20b131499
Yewande Adeyeye <Yewande Adeyeye>
2022-12-11 07:05:09 -0800

    putchar to print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *	main - print numbers using putchar
+ *
+ *
+ *
+ *
+ *	Return: 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
