f1da56321274e06631f434b2c2d6a338f53630a8
Yewande Adeyeye <Yewande Adeyeye>
2022-12-13 16:49:12 -0800

    lowercase using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *	print_alphabet - write a function that prints lowercase _putchar
+ *
+ *
+ *
+ *	Return: 0
+*/
+void print_alphabet(void)
+{
+	char ch;
+
+	if (ch >= 'a' && ch <= 'z')
+	{
+		_putchar(ch);
+	}
+	return (0);
+
+}
