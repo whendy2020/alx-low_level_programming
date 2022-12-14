2502f482fcaefdcbeed84561fec8d7a0e2d32f8a
Yewande Adeyeye <Yewande Adeyeye>
2022-12-13 21:03:00 -0800

    multiple numbers sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *	main - computes and prints sum
+ *	of 3 or 5 below 1034
+ *
+ *
+ *	Return: 0
+*/
+int main(void)
+{
+	unsigned long int sum3, sum5, sum;
+	int i;
+
+	sum = 0;
+	sum5 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; ++1)
+	{
+		if ((i % 3) == 0)
+		{
+			sum3 = sum3 + i;
+		}
+		else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
+		}
+	}
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
+	return (0);
+}
