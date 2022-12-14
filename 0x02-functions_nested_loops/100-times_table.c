dd00bb273db9462f833a8c8ef85c6d6ba8864888
Yewande Adeyeye <Yewande Adeyeye>
2022-12-13 20:50:45 -0800

    nxn times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,7 @@
 #include "main.h"
 /**
  *	print_times_table - prints n times table starting with 0
- *
  *	@n: number of the times table
- *
- *
 */
 void print_times_table(int n)
 {
@@ -20,24 +17,21 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 100)
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
+				} else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
