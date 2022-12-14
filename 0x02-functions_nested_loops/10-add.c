d1085da71cc21593a7d9061d2f3aff942502e00e
Yewande Adeyeye <Yewande Adeyeye>
2022-12-13 20:08:00 -0800

    add 2 num

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *	add - adds two integers and returns a result
+ *	@a: number 1
+ *	@b: number 2
+ *
+ *	Return: sum of a and b
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
