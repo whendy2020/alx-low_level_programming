e071db28b5fb33987ecdbf606027cd045c97fffe
Yewande Adeyeye <Yewande Adeyeye>
2022-12-13 18:26:08 -0800

    print lower case alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *	print_alphabet_x10 - print lowercase alpha 10 times
+ *
+ *
+ *
+ *
+*/
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char ch;
+
+	while (count++ <= 9)
+	{
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+			_putchar('\n');
+		}
+	}
+}
