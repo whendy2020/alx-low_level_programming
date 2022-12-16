0169798b152f6ae321d949077043512b1ba1f037
Yewande Adeyeye <Yewande Adeyeye>
2022-12-15 16:18:16 -0800

    function to multiply 2 num

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+i#include "main.h"
+/**
+ *	mul -  a and b
+ *	@a: num 1 to multiply
+ *	@b: num 2 to mulitply
+ *	Return: 0
+*/
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
