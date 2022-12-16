c603acb08222167adfb2edf112a4811e37be003e
Yewande Adeyeye <Yewande Adeyeye>
2022-12-15 17:55:06 -0800

    print 0123456789

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 /**
- *	void print_numbers - print the numbers 0 to 9
+ *	print_numbers - print the numbers 0 to 9
  *	followed by a new line
- *	@num: the value
- *
  *	Return: 0
 */
 void print_numbers(void)
 {
 	char num;
 
-	for (num = 48; num <= 58; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
 	}
-	putchar('\n');
-	return (0);
+	putchar('\n')
 }
