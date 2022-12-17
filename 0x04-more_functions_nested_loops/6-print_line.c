d9f945eff897f52a05ca4f04cc8b4ae4ab540e97
Yewande Adeyeye <Yewande Adeyeye>
2022-12-16 17:52:06 -0800

    draw a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *	print_line - printing lines
+ *	@n: number of trees
+ *
+ *	Return: void
+ *
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (1 = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
