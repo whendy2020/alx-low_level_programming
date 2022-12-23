16cf208e0fbddf506c1963687ac2719fce404e5e
Yewande Adeyeye <Yewande Adeyeye>
2022-12-23 10:21:54 -0800

    convert lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *	*string_toupper - convert lowercase letters to uppercase
+ *
+ *	@s: the letter to convert
+ *
+ *	Return: uppercase
+*/
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
