e612166334c166eab38096b28fae0ba673238c98
Yewande Adeyeye <Yewande Adeyeye>
2023-01-10 20:30:55 -0800

    creats an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,21 +8,11 @@
 */
 char *create_array(unsigned int size, char c)
 {
-	char *p;
-	unsigned int i = 0;
+	char *array = (char *)malloc(size);
 
-	if (size == 0)
+	if (size == 0 || array == 0)
 		return (NULL);
-	p = (char *) malloc(sizeof(char) * size);
-
-	if (p == NULL)
-		return (0);
-	while (i < size)
-	{
-		*(p + i) = c;
-		i++;
-	}
-	*(p + i) = '\0';
-
-	return (p);
+	while (size--)
+		array[size] = c;
+	return (array);
 }
