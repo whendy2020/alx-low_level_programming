69724613c7f74244d490c1f6aee10fb3095f044c
Yewande Adeyeye <Yewande Adeyeye>
2023-01-16 13:17:11 -0800

    a program that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *	main - program starts compiling from main
+ *	Description: this program prints the name of the file its compili
+ *
+ *	Return: 0
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
