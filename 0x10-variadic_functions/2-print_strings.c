85f1840482f8f7ac0083e15c0a476dcbede03e3e
Yewande Adeyeye <Yewande Adeyeye>
2023-01-19 16:39:09 -0800

    print_strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(ap, char *);
 		if (str)
-			print("%s", str);
+			printf("%s", str);
 		else
 			printf("(nil)");
 
