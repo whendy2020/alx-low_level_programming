03162a3239291a4728d2af7525e1f9bb12fcd283
Yewande Adeyeye <whendy.2020@gmail.com>
2023-10-16 18:37:26 -0700

    create a dynamic lib libdynamic.so

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *	_strncat - concatenates two strings
+ *	@src: the sourcw of strings
+ *	@dest: the destination of the string
+ *
+ *	@n: The length of int
+ *	Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		continue;
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
